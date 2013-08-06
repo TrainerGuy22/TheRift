86692060a547fb9591a9d967f43f8ed2901f7980
Brian Ricketts <briman94@gmail.com>
2013-08-06 17:17:41 -0500

    Working on preventing the portal from repeatedly triggering credits

--- src/minecraft/dragonborn/rift/data/RiftSavedData.java
+++ src/minecraft/dragonborn/rift/data/RiftSavedData.java
@@ -6,23 +6,27 @@ import net.minecraft.world.WorldSavedData;
 public class RiftSavedData extends WorldSavedData
 {
 	public boolean	hasDragonDied;
+	public boolean	hasDefeatedRift;
 	
 	public RiftSavedData(String name)
 	{
 		super(name);
 		this.hasDragonDied = false;
+		this.hasDefeatedRift = false;
 	}
 	
 	@Override
 	public void readFromNBT(NBTTagCompound nbt)
 	{
 		this.hasDragonDied = nbt.getBoolean("dragonDied");
+		this.hasDefeatedRift = nbt.getBoolean("defeatedRift");
 	}
 	
 	@Override
 	public void writeToNBT(NBTTagCompound nbt)
 	{
 		nbt.setBoolean("dragonDied", this.hasDragonDied);
+		nbt.setBoolean("defeatedRift", this.hasDefeatedRift);
 	}
 	
 }
